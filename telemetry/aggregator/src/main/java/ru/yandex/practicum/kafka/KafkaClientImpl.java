3ea6595f7830ab3a05653fdbef95c60e1d372e85
uncle-sly <154626220+uncle-sly@users.noreply.github.com>
2025-04-01 03:18:49 +0300

    Реализация aggregator (#3)
    * Реализация aggregator
    branch 3-aggregator
    
    * Реализация aggregator
    branch 3-aggregator
    
    * Реализация aggregator
    branch 3-aggregator
    
    * Реализация aggregator
    branch 3-aggregator
    
    * Реализация aggregator
    branch 3-aggregator
    fix
    
    ---------
    
    Co-authored-by: u-sly <150694587+u-sly@users.noreply.github.com>

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaClientImpl.java
@@ -0,0 +1,77 @@
+package ru.yandex.practicum.kafka;
+
+import deserialization.SensorEventDeserializer;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import serialization.GeneralAvroSerializer;
+
+import java.time.Duration;
+import java.util.Properties;
+
+@Component
+public class KafkaClientImpl implements KafkaClient {
+
+    private final String bootstrapServers;
+    private final String consumerGroupId;
+
+    private Producer<String, SpecificRecordBase> producer;
+    private Consumer<String, SensorEventAvro> consumer;
+
+    public KafkaClientImpl(@Value("${kafka.bootstrap.server}") String bootstrapServers,
+                           @Value("${kafka.group.id}") String consumerGroupId) {
+        this.bootstrapServers = bootstrapServers;
+        this.consumerGroupId = consumerGroupId;
+    }
+
+    @Override
+    public Producer<String, SpecificRecordBase> getProducer() {
+        if (producer == null) {
+            initProducer();
+        }
+        return producer;
+    }
+
+    private void initProducer() {
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+
+        producer = new org.apache.kafka.clients.producer.KafkaProducer<>(config);
+    }
+
+    @Override
+    public Consumer<String, SensorEventAvro> getConsumer() {
+        if (consumer == null) {
+            initConsumer();
+        }
+        return consumer;
+    }
+
+    private void initConsumer() {
+        Properties config = new Properties();
+        config.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        config.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
+        consumer = new KafkaConsumer<>(config);
+    }
+
+
+    @Override
+    public void close() {
+        if (producer != null) {
+            producer.flush();
+            producer.close(Duration.ofMillis(100));
+        }
+    }
+}
---
+++ telemetry/serialization/avro-schemas/src/main/java/deserialization/SensorEventDeserializer.java
@@ -0,0 +1,11 @@
+package deserialization;
+
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+public class SensorEventDeserializer extends BaseAvroDeserializer<SensorEventAvro> {
+
+    public SensorEventDeserializer() {
+        super(SensorEventAvro.getClassSchema());
+    }
+
+}
