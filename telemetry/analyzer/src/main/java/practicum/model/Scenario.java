a86ff5ba102745a27ea8339f8f0ec2b3ac2ee2da
u-sly <150694587+u-sly@users.noreply.github.com>
2025-04-04 03:40:46 +0300

    Реализация Analyzer branch 4-analyzer

--- telemetry/analyzer/src/main/java/practicum/model/Scenario.java
+++ telemetry/analyzer/src/main/java/practicum/model/Scenario.java
@@ -26,12 +26,12 @@ public class Scenario {
     @Column(name = "name")
     private String name;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @MapKeyColumn(table = "scenario_conditions", name = "sensor_id")
     @JoinTable(name = "scenario_conditions", joinColumns = @JoinColumn(name = "scenario_id"), inverseJoinColumns = @JoinColumn(name = "condition_id"))
     private Map<String, Condition> conditions = new HashMap<>();
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @MapKeyColumn(table = "scenario_actions", name = "sensor_id")
     @JoinTable(name = "scenario_actions", joinColumns = @JoinColumn(name = "scenario_id"), inverseJoinColumns = @JoinColumn(name = "action_id"))
     private Map<String, Action> actions = new HashMap<>();
--- telemetry/analyzer/src/main/java/practicum/service/handler/hub/ScenarioAddedEventHandler.java
+++ telemetry/analyzer/src/main/java/practicum/service/handler/hub/ScenarioAddedEventHandler.java
@@ -23,143 +23,30 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Component
 public class ScenarioAddedEventHandler implements HubEventHandler {
-//
-//    private final ScenarioRepository scenarioRepository;
-//    private final SensorRepository sensorRepository;
-//
-//    @Override
-//    public void handle(HubEventAvro hubEventAvro) {
-////        ScenarioAddedEventAvro payload = (ScenarioAddedEventAvro) hubEventAvro.getPayload();
-//
-//        if (!(hubEventAvro.getPayload() instanceof ScenarioAddedEventAvro payload)) {
-//            log.warn("Получено событие неизвестного типа: {}", hubEventAvro.getPayload().getClass().getName());
-//            return;
-//        }
-//
-//        log.info("Добавлен сценарий: {}", payload);
-//
-//        if (scenarioRepository.findByHubIdAndName(hubEventAvro.getHubId(), payload.getName()).isEmpty()) {
-//            Scenario scenario = Scenario.builder()
-//                    .hubId(hubEventAvro.getHubId())
-//                    .name(payload.getName())
-//                    .conditions(toConditions(payload.getConditions(), hubEventAvro.getHubId()))
-//                    .actions(toActions(payload.getActions(), hubEventAvro.getHubId()))
-//                    .build();
-//            scenarioRepository.save(scenario);
-//            log.info("Сценарий '{}' успешно сохранен", payload.getName());
-//        }
-//    }
-//
-//    @Override
-//    public String getType() {
-//        return ScenarioAddedEventAvro.class.getName();
-//    }
-//
-//    private Map<String, Condition> toConditions(List<ScenarioConditionAvro> conditions, String hubId) {
-//        validateSensorsExist(conditions.stream().map(ScenarioConditionAvro::getSensorId).toList(), hubId);
-//
-//        return conditions.stream().collect(Collectors.toMap(
-//                ScenarioConditionAvro::getSensorId,
-//                condition -> Condition.builder()
-//                        .type(condition.getType())
-//                        .operation(condition.getOperation())
-//                        .value(toValue(condition.getValue()))
-//                        .build()
-//        ));
-//    }
-//
-//    private Map<String, Action> toActions(List<DeviceActionAvro> actions, String hubId) {
-//        validateSensorsExist(actions.stream().map(DeviceActionAvro::getSensorId).toList(), hubId);
-//
-//        return actions.stream().collect(Collectors.toMap(
-//                DeviceActionAvro::getSensorId,
-//                action -> Action.builder()
-//                        .type(action.getType())
-//                        .value(action.getValue() == null ? 0 : action.getValue())
-//                        .build()
-//        ));
-//    }
-//
-//    private void validateSensorsExist(List<String> sensorIds, String hubId) {
-//        if (!sensorRepository.existsByIdInAndHubId(sensorIds, hubId)) {
-//            log.error("Некоторые сенсоры не найдены для hubId={} и ids={}", hubId, sensorIds);
-//            throw new NoSuchElementException("Некоторые элементы не найдены");
-//        }
-//    }
-//
-////    private Map<String, Condition> toConditions(List<ScenarioConditionAvro> conditions, String hubId) {
-////        List<String> ids = conditions.stream()
-////                .map(ScenarioConditionAvro::getSensorId)
-////                .toList();
-////        if (sensorRepository.existsByIdInAndHubId(ids, hubId)) {
-////            return conditions.stream()
-////                    .collect(Collectors.toMap(
-////                            ScenarioConditionAvro::getSensorId,
-////                            condition -> Condition.builder()
-////                                    .type(condition.getType())
-////                                    .operation(condition.getOperation())
-////                                    .value(toValue(condition.getValue()))
-////                                    .build()
-////                    ));
-////        } else {
-////            throw new NoSuchElementException("Элемент не найден");
-////        }
-////    }
-////
-////    private Map<String, Action> toActions(List<DeviceActionAvro> actions, String hubId) {
-////        List<String> ids = actions.stream()
-////                .map(DeviceActionAvro::getSensorId)
-////                .toList();
-////        if (sensorRepository.existsByIdInAndHubId(ids, hubId)) {
-////            return actions.stream()
-////                    .collect(Collectors.toMap(
-////                            DeviceActionAvro::getSensorId,
-////                            action -> Action.builder()
-////                                    .type(action.getType())
-////                                    .value(action.getValue() == null ? 0 : action.getValue())
-////                                    .build()
-////                    ));
-////        } else {
-////            throw new NoSuchElementException("Элемент не найден");
-////        }
-////    }
-//
-////    private Integer toValue(Object value) {
-////        if (value instanceof Integer) {
-////            return (Integer) value;
-////        } else if (value instanceof Boolean) {
-////            if ((Boolean) value) {
-////                return 1;
-////            }
-////        }
-////        return 0;
-////    }
-//
-//    private Integer toValue(Object value) {
-//        if (value instanceof Integer intValue) {
-//            return intValue;
-//        } else if (value instanceof Boolean boolValue) {
-//            return boolValue ? 1 : 0;
-//        }
-//        return 0;
-//    }
 
-
-private final ScenarioRepository scenarioRepository;
+    private final ScenarioRepository scenarioRepository;
     private final SensorRepository sensorRepository;
 
     @Override
-    public void handle(HubEventAvro event) {
-        ScenarioAddedEventAvro payload = (ScenarioAddedEventAvro) event.getPayload();
-        log.info("Scenario added: {}", payload);
-        if (scenarioRepository.findByHubIdAndName(event.getHubId(), payload.getName()).isEmpty()) {
+    public void handle(HubEventAvro hubEventAvro) {
+//        ScenarioAddedEventAvro payload = (ScenarioAddedEventAvro) hubEventAvro.getPayload();
+
+        if (!(hubEventAvro.getPayload() instanceof ScenarioAddedEventAvro payload)) {
+            log.warn("Получено событие неизвестного типа: {}", hubEventAvro.getPayload().getClass().getName());
+            return;
+        }
+
+        log.info("Добавлен сценарий: {}", payload);
+
+        if (scenarioRepository.findByHubIdAndName(hubEventAvro.getHubId(), payload.getName()).isEmpty()) {
             Scenario scenario = Scenario.builder()
-                    .hubId(event.getHubId())
+                    .hubId(hubEventAvro.getHubId())
                     .name(payload.getName())
-                    .conditions(mapToConditions(payload.getConditions(), event.getHubId()))
-                    .actions(mapToActions(payload.getActions(), event.getHubId()))
+                    .conditions(toConditions(payload.getConditions(), hubEventAvro.getHubId()))
+                    .actions(toActions(payload.getActions(), hubEventAvro.getHubId()))
                     .build();
             scenarioRepository.save(scenario);
+            log.info("Сценарий '{}' успешно сохранен", payload.getName());
         }
     }
 
@@ -168,53 +55,95 @@ private final ScenarioRepository scenarioRepository;
         return ScenarioAddedEventAvro.class.getName();
     }
 
-    private Map<String, Condition> mapToConditions(List<ScenarioConditionAvro> conditions, String hubId) {
-        List<String> ids = conditions.stream()
-                .map(ScenarioConditionAvro::getSensorId)
-                .toList();
-        if (sensorRepository.existsByIdInAndHubId(ids, hubId)) {
-            return conditions.stream()
-                    .collect(Collectors.toMap(
-                            ScenarioConditionAvro::getSensorId,
-                            condition -> Condition.builder()
-                                    .type(condition.getType())
-                                    .operation(condition.getOperation())
-                                    .value(mapToValue(condition.getValue()))
-                                    .build()
-                    ));
-        } else {
-            throw new NoSuchElementException("No value present");
+    private Map<String, Condition> toConditions(List<ScenarioConditionAvro> conditions, String hubId) {
+        validateSensorsExist(conditions.stream().map(ScenarioConditionAvro::getSensorId).toList(), hubId);
+
+        return conditions.stream().collect(Collectors.toMap(
+                ScenarioConditionAvro::getSensorId,
+                condition -> Condition.builder()
+                        .type(condition.getType())
+                        .operation(condition.getOperation())
+                        .value(toValue(condition.getValue()))
+                        .build()
+        ));
+    }
+
+    private Map<String, Action> toActions(List<DeviceActionAvro> actions, String hubId) {
+        validateSensorsExist(actions.stream().map(DeviceActionAvro::getSensorId).toList(), hubId);
+
+        return actions.stream().collect(Collectors.toMap(
+                DeviceActionAvro::getSensorId,
+                action -> Action.builder()
+                        .type(action.getType())
+                        .value(action.getValue() == null ? 0 : action.getValue())
+                        .build()
+        ));
+    }
+
+    private void validateSensorsExist(List<String> sensorIds, String hubId) {
+        if (!sensorRepository.existsByIdInAndHubId(sensorIds, hubId)) {
+            log.error("Некоторые сенсоры не найдены для hubId={} и ids={}", hubId, sensorIds);
+            throw new NoSuchElementException("Некоторые элементы не найдены");
         }
     }
 
-    private Integer mapToValue(Object value) {
-        if (value instanceof Integer) {
-            return (Integer) value;
-        } else if (value instanceof Boolean) {
-            if ((Boolean) value) {
-                return 1;
-            }
+//    private Map<String, Condition> toConditions(List<ScenarioConditionAvro> conditions, String hubId) {
+//        List<String> ids = conditions.stream()
+//                .map(ScenarioConditionAvro::getSensorId)
+//                .toList();
+//        if (sensorRepository.existsByIdInAndHubId(ids, hubId)) {
+//            return conditions.stream()
+//                    .collect(Collectors.toMap(
+//                            ScenarioConditionAvro::getSensorId,
+//                            condition -> Condition.builder()
+//                                    .type(condition.getType())
+//                                    .operation(condition.getOperation())
+//                                    .value(toValue(condition.getValue()))
+//                                    .build()
+//                    ));
+//        } else {
+//            throw new NoSuchElementException("Элемент не найден");
+//        }
+//    }
+//
+//    private Map<String, Action> toActions(List<DeviceActionAvro> actions, String hubId) {
+//        List<String> ids = actions.stream()
+//                .map(DeviceActionAvro::getSensorId)
+//                .toList();
+//        if (sensorRepository.existsByIdInAndHubId(ids, hubId)) {
+//            return actions.stream()
+//                    .collect(Collectors.toMap(
+//                            DeviceActionAvro::getSensorId,
+//                            action -> Action.builder()
+//                                    .type(action.getType())
+//                                    .value(action.getValue() == null ? 0 : action.getValue())
+//                                    .build()
+//                    ));
+//        } else {
+//            throw new NoSuchElementException("Элемент не найден");
+//        }
+//    }
+
+//    private Integer toValue(Object value) {
+//        if (value instanceof Integer) {
+//            return (Integer) value;
+//        } else if (value instanceof Boolean) {
+//            if ((Boolean) value) {
+//                return 1;
+//            }
+//        }
+//        return 0;
+//    }
+
+    private Integer toValue(Object value) {
+        if (value instanceof Integer intValue) {
+            return intValue;
+        } else if (value instanceof Boolean boolValue) {
+            return boolValue ? 1 : 0;
         }
         return 0;
     }
 
-    private Map<String, Action> mapToActions(List<DeviceActionAvro> actions, String hubId) {
-        List<String> ids = actions.stream()
-                .map(DeviceActionAvro::getSensorId)
-                .toList();
-        if (sensorRepository.existsByIdInAndHubId(ids, hubId)) {
-            return actions.stream()
-                    .collect(Collectors.toMap(
-                            DeviceActionAvro::getSensorId,
-                            action -> Action.builder()
-                                    .type(action.getType())
-                                    .value(action.getValue() == null ? 0 : action.getValue())
-                                    .build()
-                    ));
-        } else {
-            throw new NoSuchElementException("No value present");
-        }
-    }
 
 
 }
