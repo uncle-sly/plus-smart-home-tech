8968ac57dcf684532321ddee3bbf5b0947846d3d
u-sly <150694587+u-sly@users.noreply.github.com>
2025-05-01 18:23:33 +0300

    branch 8-gateway fix after review branch 7

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/mapper/ShoppingCartMapper.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/mapper/ShoppingCartMapper.java
@@ -7,8 +7,6 @@ import ru.yandex.practicum.model.ShoppingCart;
 @Mapper(componentModel = "spring")
 public interface ShoppingCartMapper {
 
-//    ShoppingCart mapToShoppingCart(ShoppingCartDto shoppingCartDto);
-
     ShoppingCartDto mapToShoppingCartDto(ShoppingCart shoppingCart);
 
 }
